e9f69a606500f2487c8bfe16c6601de1137f0d10
koki sugano <c9xj.c9ajmf2@gmail.com>
2026-06-27 01:13:39 +0900

    chapter11

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@
 // import Header from "./Header";
 // import MainContent from "./MainContent";
 // import GreetingSection from "./GreetingSection";
-import CardExample from "./CardExample";
+// import CardExample from "./CardExample";
+// import ClickExample from "./ClickExample";
+// import InputExample from "./InputExample";
+// import EventExample from "./EventExample";
+
+import FormExample from "./FormExample";
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <CardExample />
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-center py-8">
+          preventDefault()の例
+        </h1>
+        <FormExample />
+      </div>
     </div>
   );
 }
