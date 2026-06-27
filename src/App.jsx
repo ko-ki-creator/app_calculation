389f77326909d5926e20d7b217593437521df507
koki sugano <c9xj.c9ajmf2@gmail.com>
2026-06-27 15:18:00 +0900

    chapter13

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Counter from "./Counter";
+import MessageApp from "./MessageApp";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold text-center mb-8">
-          React State の練習
+          文字列Stateの練習
         </h1>
-        <Counter />
+        <MessageApp />
       </div>
     </div>
   );
