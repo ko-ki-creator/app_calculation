33f822a367f784f7e7657cc68ca39f3343c6b0f4
koki sugano <c9xj.c9ajmf2@gmail.com>
2026-06-27 17:40:14 +0900

    chapter14

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import MessageApp from "./MessageApp";
+import Timer from "./TImer";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold text-center mb-8">
-          文字列Stateの練習
+          タイマー機能
         </h1>
-        <MessageApp />
+        <Timer />
       </div>
     </div>
   );
